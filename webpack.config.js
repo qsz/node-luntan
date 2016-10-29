14ab447b7767fa9cc7b431a8bfc3722eff81e6be
junjie xu <junjie xu>
2016-10-29 23:28:07 +0800

    change sth

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     },
 
     devServer: {
-        contentBase: "./",//本地服务器所加载的页面所在的目录
+        contentBase: "./public",//本地服务器所加载的页面所在的目录
         colors: true,//终端中输出结果为彩色
         historyApiFallback: true,//不跳转
         inline: true,             //实时刷新
